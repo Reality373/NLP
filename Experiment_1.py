365a2d6d3cb351c184f4f12a0e40e5985e382554
Reality <15974saif@gmail.com>
2026-01-30 10:16:25 +0530

    Experient1 for NLP

---
+++ Experiment_1.py
@@ -0,0 +1,21 @@
+# Removal of Stop_words
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# Download the stopwords from NLTK
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('punkt_tab')
+
+# Sample text
+text = "How to remove stopwords using nltk"
+print("Original Text:", text)
+
+# Tokenize the text
+tokens=word_tokenize(text.lower())
+stop_words=set(stopwords.words('english'))
+filtered_words=[word for word in tokens if word not in stop_words]
+print("Token", tokens)
+print("After stopwords :", filtered_words)
+
